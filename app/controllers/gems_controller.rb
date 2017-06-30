e44769b5177e90f70b1e7319026761a3c6368f7e
Madeline LeClair <madelineleclair@gmail.com>
2017-06-30 18:18:49 -0400

    Begin making display information for found gem

--- app/controllers/gems_controller.rb
+++ app/controllers/gems_controller.rb
@@ -1,5 +1,10 @@
 class GemsController < ApplicationController
   def index
+    search_term = params["gem"]["search_term"]
+    @gem = Gems.info "#{search_term.downcase}"
     debugger
+    dependencies = @gem['dependencies']
+    @dependencies = dependencies["development"] + dependencies["runtime"]
+    render :index
   end
 end
